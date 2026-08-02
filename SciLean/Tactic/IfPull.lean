dc8cf2178798a3e204b7acccf3238b944e53afe7
Sankalp Thakur <31366524+sankalpsthakur@users.noreply.github.com>
2026-08-02 07:42:53 +0530

    fix: generate proofs for if_pull rewrites

--- SciLean/Tactic/IfPull.lean
+++ SciLean/Tactic/IfPull.lean
@@ -1,83 +1,125 @@
 import Lean
 import SciLean.Util.RewriteBy
-import SciLean.Util.SorryProof
 
 namespace SciLean.Tactic
 
 open Lean Meta
 
+universe u v
+
 initialize registerTraceClass `Meta.Tactic.if_pull
+
+private theorem apply_ite' {α : Sort u} {β : Sort v} (f : α → β)
+    (c : Prop) [Decidable c] (t e : α) :
+    f (if c then t else e) = if c then f t else f e := by
+  by_cases h : c <;> simp [h]
+
+private theorem ite_apply' {α : Sort u} {σ : α → Sort v}
+    (c : Prop) [Decidable c] (t e : (a : α) → σ a) (a : α) :
+    (if c then t else e) a = if c then t a else e a := by
+  by_cases h : c <;> simp [h]
+
+private def mkFunExtN (xs : Array Expr) (proof : Expr) : MetaM Expr := do
+  let mut proof := proof
+  for x in xs.reverse do
+    proof ← mkFunExt (← mkLambdaFVars #[x] proof)
+  return proof
 
 /-- Simproc that pulls `if` out of applications and lambda functions.
 
-For example
-  - `(if 0 ≤ x then x else -x) + y` is transformed to `if 0 ≤ x then x + y else -x + y`
-  - `fun y => if 0 ≤ x then y + x else y - x` is transformed to `if 0 ≤ x then (fun y => y + x) else (fun y => y - x)`  -/
+For example:
+- `(if 0 ≤ x then x else -x) + y` is transformed to
+  `if 0 ≤ x then x + y else -x + y`.
+- `fun y => if 0 ≤ x then y + x else y - x` is transformed to
+  `if 0 ≤ x then (fun y => y + x) else (fun y => y - x)`. -/
 simproc_decl if_pull (_) := fun e => do
-
   match e with
   | .app .. =>
     let fn := e.getAppFn
     let args := e.getAppArgs
 
-    let mut thn : Expr := default
-    let mut els : Expr := default
-    let mut cond : Expr := default
-
-    -- do not pull `if` out of `if`
-    -- this would cause infinite loops
     if e.isAppOf ``ite then
+      -- Do not pull an unapplied `if` out of itself. This would cause an infinite loop.
       if args.size ≤ 5 then
         return .continue
-      else
-        thn := mkAppN (e.getArg! 3) args[5:]
-        els := mkAppN (e.getArg! 4) args[5:]
-        cond := e.getArg! 1
-    else
-      -- locate argument with if statement
-      let .some i := args.findIdx? fun arg => arg.isAppOfArity ``ite 5
-        | return .continue
 
-      let arg := args[i]!
-      -- todo: introduce let bindings for other arguments (probaly only for non-type arguments)
-      thn := mkAppN fn (args.set! i (arg.getArg! 3))
-      els := mkAppN fn (args.set! i (arg.getArg! 4))
-      cond := arg.getArg! 1
+      let α := e.getArg! 0
+      let cond := e.getArg! 1
+      let inst := e.getArg! 2
+      let t := e.getArg! 3
+      let f := e.getArg! 4
+      let extraArgs := args[5:]
+      let thn := mkAppN t extraArgs
+      let els := mkAppN f extraArgs
+      let e' ← mkAppOptM ``ite #[none, cond, inst, thn, els]
+      let context ← withLocalDeclD `branch α fun branch =>
+        mkLambdaFVars #[branch] (mkAppN branch extraArgs)
+      let proof ← mkAppOptM ``apply_ite' #[α, none, context, cond, inst, t, f]
 
-    let e' ← mkAppOptM ``ite #[none, cond, none, thn, els]
+      trace[Meta.Tactic.if_pull] s!"if_pull: \n{← ppExpr e}\n==>\n{← ppExpr e'}\n"
+      return .visit { expr := e', proof? := some proof }
 
-    let prf := Expr.app (Expr.const ``SciLean.sorryProofAxiom []) (← mkEq e e')
-
-    trace[Meta.Tactic.if_pull] s!"if_pull: \n{← ppExpr e}\n==>\n{← ppExpr e'}\n"
-    return .visit { expr := e', proof? := prf }
-
-  | .lam ..
-  | .letE .. =>
-
-    lambdaLetTelescope e fun xs b => do
-
-    unless b.isAppOfArity ``ite 5 do return .continue
-    let cond := b.getArg! 1
-    let i := xs.reverse.findIdx? (fun x => cond.containsFVar x.fvarId!) |>.getD xs.size
-
-    unless i > 0 do return .continue
-
-    let xs' := xs[0:xs.size-i]
-    let xs'' := xs[xs.size-i:]
-
-    let thn ← mkLambdaFVars xs'' (b.getArg! 3)
-    let els ← mkLambdaFVars xs'' (b.getArg! 4)
-
-    let e' ← mkAppOptM ``ite #[none, b.getArg! 1, none, thn, els]
-    let e' ← mkLambdaFVars xs' e'
-
-    let prf := Expr.app (Expr.const ``SciLean.sorryProofAxiom []) (← mkEq e e')
+    -- Locate an argument containing an `if` statement.
+    let some i := args.findIdx? fun arg => arg.isAppOfArity ``ite 5
+      | return .continue
+    let arg := args[i]!
+    let α := arg.getArg! 0
+    let cond := arg.getArg! 1
+    let inst := arg.getArg! 2
+    let t := arg.getArg! 3
+    let f := arg.getArg! 4
+    let thn := mkAppN fn (args.set! i t)
+    let els := mkAppN fn (args.set! i f)
+    let e' ← mkAppOptM ``ite #[none, cond, inst, thn, els]
+    let context ← withLocalDeclD `branch α fun branch =>
+      mkLambdaFVars #[branch] (mkAppN fn (args.set! i branch))
+    let proof ← mkAppOptM ``apply_ite' #[α, none, context, cond, inst, t, f]
 
     trace[Meta.Tactic.if_pull] s!"if_pull: \n{← ppExpr e}\n==>\n{← ppExpr e'}\n"
-    return .visit { expr := e', proof? := prf }
+    return .visit { expr := e', proof? := some proof }
 
+  | .lam .. =>
+    lambdaTelescope e fun xs b => do
+      unless b.isAppOfArity ``ite 5 do
+        return .continue
+
+      let cond := b.getArg! 1
+      let inst := b.getArg! 2
+      let i := xs.reverse.findIdx? (fun x => cond.containsFVar x.fvarId!) |>.getD xs.size
+      unless i > 0 do
+        return .continue
+
+      let xs' := xs[0 : xs.size - i]
+      let xs'' := xs[xs.size - i :]
+      let thn ← mkLambdaFVars xs'' (b.getArg! 3)
+      let els ← mkLambdaFVars xs'' (b.getArg! 4)
+      let inner ← mkAppOptM ``ite #[none, cond, inst, thn, els]
+      let e' ← mkLambdaFVars xs' inner
+
+      -- Repeated `ite_apply'` steps show that applying the pulled-out `if` to the
+      -- trailing binders gives the original body. Function extensionality then
+      -- reconstructs the two lambda telescopes.
+      let mut proof ← mkEqRefl inner
+      let mut thnApp := thn
+      let mut elsApp := els
+      for x in xs'' do
+        proof ← mkCongrFun proof x
+        let step ← mkAppOptM ``ite_apply' #[none, none, cond, inst, thnApp, elsApp, x]
+        proof ← mkEqTrans proof step
+        thnApp := mkApp thnApp x
+        elsApp := mkApp elsApp x
+      proof ← mkEqSymm proof
+      proof ← mkFunExtN xs'' proof
+      proof ← mkFunExtN xs' proof
+
+      trace[Meta.Tactic.if_pull] s!"if_pull: \n{← ppExpr e}\n==>\n{← ppExpr e'}\n"
+      return .visit { expr := e', proof? := some proof }
+
+  -- Pulling an `if` through a `let` body needs a separate congruence proof. Until
+  -- that proof is implemented, leave let-expressions unchanged rather than using
+  -- an axiom to justify the rewrite.
+  | .letE .. => return .continue
   | _ => return .continue
-
 
 
 -- #check ((if 0 < 1 then (fun x : Float => x + 2) else (fun x : Float => x + 3)) 42).log rewrite_by
